0771e55ba63350f6a00a60f89c3099950c58c3f3
santio89 <santiolais1989@gmail.com>
2023-08-09 14:52:48 -0300

    small fix

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -13,7 +13,6 @@ export default function Nav({ rootTheme }) {
   }
 
   useEffect(() => {
-    console.log(darkTheme)
     if (firstRender.current === true) {
       firstRender.current = false;
       return
