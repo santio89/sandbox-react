ef0f2f6a71f9a79ba9de1ff5bfc55a79c5f93b80
santio89 <santiolais1989@gmail.com>
2023-08-12 15:25:24 -0300

    small fix

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <>
-      <div className={`root-theme ${darkTheme ? 'dark-theme' : ""}`}>
+      <div className={`root-theme ${darkTheme ? 'dark-theme' : "light-theme"}`}>
         <BrowserRouter>
           <Nav />
           <Routes>
