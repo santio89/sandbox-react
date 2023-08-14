a6e40810c1ce48655af669dca0962a5ba0c7ad43
santio89 <santiolais1989@gmail.com>
2023-08-13 22:48:51 -0300

    small fix

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -117,11 +117,13 @@ export default function Nav({ rootTheme }) {
         <Link className="main__header__site" to={"/"}><h1>SandBox|HTML-CSS-JS</h1></Link>
       </nav>
       <div className="main__header__buttons">
-        <button onClick={() => { toggleModal() }}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="diamond-fill" viewBox="0 0 16 16">
-            <path d="M9.05.435c-.58-.58-1.52-.58-2.1 0L4.047 3.339 8 7.293l3.954-3.954L9.049.435zm3.61 3.611L8.708 8l3.954 3.954 2.904-2.905c.58-.58.58-1.519 0-2.098l-2.904-2.905zm-.706 8.614L8 8.708l-3.954 3.954 2.905 2.904c.58.58 1.519.58 2.098 0l2.905-2.904zm-8.614-.706L7.292 8 3.339 4.046.435 6.951c-.58.58-.58 1.519 0 2.098l2.904 2.905z" />
-          </svg>
-        </button>
+        <div className="snippets-box">
+          <button onClick={() => { toggleModal() }}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="diamond-fill" viewBox="0 0 16 16">
+              <path d="M9.05.435c-.58-.58-1.52-.58-2.1 0L4.047 3.339 8 7.293l3.954-3.954L9.049.435zm3.61 3.611L8.708 8l3.954 3.954 2.904-2.905c.58-.58.58-1.519 0-2.098l-2.904-2.905zm-.706 8.614L8 8.708l-3.954 3.954 2.905 2.904c.58.58 1.519.58 2.098 0l2.905-2.904zm-8.614-.706L7.292 8 3.339 4.046.435 6.951c-.58.58-.58 1.519 0 2.098l2.904 2.905z" />
+            </svg>
+          </button>
+        </div>
         <div className='theme-mode'>
           {!darkTheme ?
             <button onClick={() => toggleDarkTheme()}>
