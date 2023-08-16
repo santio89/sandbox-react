c2c4d2e3c00d06ff8b138b18775557f2da2fb7aa
santio89 <santiolais1989@gmail.com>
2023-08-16 13:05:26 -0300

    adding beautify

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,9 @@ import { setCodeHtml, setCodeCss, setCodeJs } from "../store/actions/code.action
 import { Link } from "react-router-dom";
 import Prism from 'prismjs'
 import '../styles/css/prism.css'
+import { html_beautify } from "js-beautify";
+import { css_beautify } from "js-beautify";
+import { js_beautify } from "js-beautify";
 
 
 export default function Home() {
@@ -59,6 +62,27 @@ export default function Home() {
                 return
         }
         setClearConfirm(false);
+    }
+    const beautify = () => {
+        switch (tabActive) {
+            case 'html': {
+                const beautify = html_beautify(html)
+                setHtml(beautify)
+                break;
+            }
+            case 'css': {
+                const beautify = css_beautify(css)
+                setCss(beautify)
+                break;
+            }
+            case 'js': {
+                const beautify = js_beautify(js)
+                setJs(beautify)
+                break;
+            }
+            default:
+                return
+        }
     }
 
     const syncScroll = () => {
@@ -316,6 +340,12 @@ export default function Home() {
                             <span>{tabActive.toUpperCase()}</span>
                         </span>
                         <span className="mainCode__input__type__btnWrapper">
+                            <button className="mainCode__input__type__format" title="Format code" onClick={() => { beautify() }}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi-code-square" viewBox="0 0 16 16">
+                                    <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
+                                    <path d="M6.854 4.646a.5.5 0 0 1 0 .708L4.207 8l2.647 2.646a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 0 1 .708 0zm2.292 0a.5.5 0 0 0 0 .708L11.793 8l-2.647 2.646a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708 0z" />
+                                </svg>
+                            </button>
                             {
                                 copyClipboard ?
                                     <button className="mainCode__input__type__clip" title="Copy to clipboard" onClick={() => { copyToClipboard() }}>
