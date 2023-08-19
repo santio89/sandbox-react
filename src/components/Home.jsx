ceb49e340d398dcd58872dab3972f7ba99d06180
santio89 <santiolais1989@gmail.com>
2023-08-18 23:11:20 -0300

    small fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -470,7 +470,7 @@ export default function Home() {
                         <span className="mainCode__input__type__btnWrapper">
                             {
                                 <button className="mainCode__input__type__format" title="Format code (alt+shift+f)" onClick={() => { beautify() }}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi-code" viewBox="0 0 16 16">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi-code" viewBox="0 0 16 16">
                                         <path d="M5.854 4.854a.5.5 0 1 0-.708-.708l-3.5 3.5a.5.5 0 0 0 0 .708l3.5 3.5a.5.5 0 0 0 .708-.708L2.707 8l3.147-3.146zm4.292 0a.5.5 0 0 1 .708-.708l3.5 3.5a.5.5 0 0 1 0 .708l-3.5 3.5a.5.5 0 0 1-.708-.708L13.293 8l-3.147-3.146z" />
                                     </svg>
                                 </button>
