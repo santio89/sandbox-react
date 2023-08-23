7b392565de9285be807be7d3e68db6e6864900bf
santio89 <santiolais1989@gmail.com>
2023-08-23 18:51:11 -0300

    adding-split-pane

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import Home from "./components/Home";
 import Nav from "./components/Nav";
+import './styles/css/prism.css'
 import './styles/css/styles.css'
 import { useRef } from "react";
 import { useDispatch } from "react-redux";
