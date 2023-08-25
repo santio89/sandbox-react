a08b989a2943ec919a968e1e71fa81b8c2f5438c
santio89 <santiolais1989@gmail.com>
2023-08-25 19:15:51 -0300

    small fix

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -182,7 +182,7 @@ export default function Modal({ callbacks }) {
                                                                             <span className="presets__option__confirm" onClick={e => e.stopPropagation()}>
                                                                                 <span>Rename snippet?</span>
                                                                                 <form onSubmit={(e) => { e.preventDefault(); editSelectedPreset(preset.id, editName) }}>
-                                                                                    <input type="text" value={editName} onChange={e => { setEditName(e.target.value) }} placeholder="NEW NAME" />
+                                                                                    <input type="text" value={editName} onChange={e => { setEditName(e.target.value) }} onKeyUp={(e) => e.stopPropagation()} placeholder="NEW NAME" />
                                                                                     <span className="presets__option__confirm__buttons">
                                                                                         <span onClick={(e) => { e.stopPropagation(); editSelectedPreset(preset.id, editName) }}>Yes</span>
                                                                                         <span onClick={(e) => { e.stopPropagation(); setEditId(null) }}>No</span>
