3a959fc5156f9f9a0207fb15ec4b51eac702251e
santio89 <santiolais1989@gmail.com>
2023-09-14 15:40:51 -0300

    small fix

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -57,7 +57,7 @@ export default function Nav({ rootTheme }) {
       </nav>
       <div className="main__header__buttons">
         <div className="snippets-box">
-          <button onClick={() => { openModal() }}>
+          <button onClick={() => { openModal() }} title="Manage">
             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="diamond-fill" viewBox="0 0 16 16">
               <path d="M9.05.435c-.58-.58-1.52-.58-2.1 0L4.047 3.339 8 7.293l3.954-3.954L9.049.435zm3.61 3.611L8.708 8l3.954 3.954 2.904-2.905c.58-.58.58-1.519 0-2.098l-2.904-2.905zm-.706 8.614L8 8.708l-3.954 3.954 2.905 2.904c.58.58 1.519.58 2.098 0l2.905-2.904zm-8.614-.706L7.292 8 3.339 4.046.435 6.951c-.58.58-.58 1.519 0 2.098l2.904 2.905z" />
             </svg>
@@ -65,12 +65,12 @@ export default function Nav({ rootTheme }) {
         </div>
         <div className='theme-mode'>
           {!darkTheme ?
-            <button onClick={() => toggleDarkTheme()}>
+            <button onClick={() => toggleDarkTheme()} title="Toggle theme">
               <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="sun-fill" viewBox="0 0 16 16">
                 <path d="M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z" />
               </svg>
             </button> :
-            <button onClick={() => toggleDarkTheme()}>
+            <button onClick={() => toggleDarkTheme()} title="Toggle theme">
               <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="moon-fill" viewBox="0 0 16 16">
                 <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z" />
               </svg>
@@ -88,7 +88,7 @@ export default function Nav({ rootTheme }) {
                 </span>
               </span>
               :
-              <button className="create-new" onClick={() => { setNewProject(true); }}>
+              <button className="create-new" onClick={() => { setNewProject(true); }} title="Create new">
                 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi-plus-lg" viewBox="0 0 16 16">
                   <path fillRule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z" />
                 </svg>
