c6fa84ba7ec99af573942c07a7fc897e5bb325ef
santio89 <santiolais1989@gmail.com>
2023-10-27 13:46:02 -0300

    feat-name

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -452,6 +452,7 @@ export default function Modal({ callbacks }) {
                                                                                     <path d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5z" />
                                                                                 </svg>
                                                                             </button>
+                                                                            <span className="presets__option__main__feat" title={`Featured: ${preset.featName}`}>ft. {preset.featName}</span>
                                                                         </span>
                                                                         <span className="presets__option__main__name" tabIndex={0} role="button" disabled={loaded || renamePresetLoader || shared || saved || deletePresetLoader} onClick={() => {
                                                                             setEditName("");
