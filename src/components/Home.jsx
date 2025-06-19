69fdad2d1c85403459231af44216111db2ea94e4
santio89 <santiolais1989@gmail.com>
2025-06-19 10:46:51 -0300

    small-fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -584,13 +584,13 @@ export default function Home({ sharedSnippetHome }) {
                                             <span className="mainCode__output__type__btnWrapper">
                                                 {
                                                     downloadCode ?
-                                                        <Link to={downloadUrl} target="_blank" download title="Download code output" onClick={() => setDownloadCode(true)}>
+                                                        <Link to={downloadUrl} target="_blank" download="sandbox-code-output" title="Download code output" onClick={() => setDownloadCode(true)}>
                                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi-file-check-fill" viewBox="0 0 16 16">
                                                                 <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm-1.146 6.854-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 8.793l2.646-2.647a.5.5 0 0 1 .708.708z" />
                                                             </svg>
                                                         </Link>
                                                         :
-                                                        <Link to={downloadUrl} target="_blank" download title="Download code output" onClick={() => setDownloadCode(true)}>
+                                                        <Link to={downloadUrl} target="_blank" download="sandbox-code-output" title="Download code output" onClick={() => setDownloadCode(true)}>
                                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi-file-arrow-down-fill" viewBox="0 0 16 16">
                                                                 <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM8 5a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 1 1 .708-.708L7.5 9.293V5.5A.5.5 0 0 1 8 5z" />
                                                             </svg>
@@ -716,13 +716,13 @@ export default function Home({ sharedSnippetHome }) {
                                             <span className="mainCode__output__type__btnWrapper">
                                                 {
                                                     downloadCode ?
-                                                        <Link to={downloadUrl} target="_blank" download title="Download code output" onClick={() => setDownloadCode(true)}>
+                                                        <Link to={downloadUrl} target="_blank" download="sandbox-code-output" title="Download code output" onClick={() => setDownloadCode(true)}>
                                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi-file-check-fill" viewBox="0 0 16 16">
                                                                 <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm-1.146 6.854-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 8.793l2.646-2.647a.5.5 0 0 1 .708.708z" />
                                                             </svg>
                                                         </Link>
                                                         :
-                                                        <Link to={downloadUrl} target="_blank" download title="Download code output" onClick={() => setDownloadCode(true)}>
+                                                        <Link to={downloadUrl} target="_blank" download="sandbox-code-output" title="Download code output" onClick={() => setDownloadCode(true)}>
                                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi-file-arrow-down-fill" viewBox="0 0 16 16">
                                                                 <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM8 5a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 1 1 .708-.708L7.5 9.293V5.5A.5.5 0 0 1 8 5z" />
                                                             </svg>
