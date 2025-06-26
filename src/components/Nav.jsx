10e7018c929a28743a849b792ac11f1ef64e52b5
santio89 <santiolais1989@gmail.com>
2025-06-26 18:33:41 -0300

    small-fix

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -89,7 +89,7 @@ export default function Nav({ rootTheme }) {
               </span>
               :
               <button className="create-new" onClick={() => { setNewProject(true); }} title="Create new">
-                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi-plus-lg" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi-plus-lg" viewBox="0 0 16 16">
                   <path fillRule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z" />
                 </svg>
               </button>
