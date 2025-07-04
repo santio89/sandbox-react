9029c7575478c76760083f4ac6491381c7cb5237
santio89 <santiolais1989@gmail.com>
2025-07-04 18:28:04 -0300

    adding-dropdown-styles

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -12,6 +12,7 @@ import { useNavigate } from "react-router-dom";
 import Draggable from "react-draggable";
 import { objectEquality } from "../utils/objectEquality"
 import { use } from "react";
+import Dropdown from "./Dropdown";
 
 export default function Modal({ callbacks }) {
     const dispatch = useDispatch();
@@ -40,6 +41,8 @@ export default function Modal({ callbacks }) {
     const currentSnippet = useSelector(state => state.theme.currentSnippet)
 
     const [saveTab, setSaveTab] = useState(currentSnippet ? "current" : "new")
+
+    const [dropdownOpen, setDropdownOpen] = useState(false)
 
     const { setNewProject } = callbacks;
 
@@ -390,6 +393,44 @@ export default function Modal({ callbacks }) {
                                                 return (
                                                     <div draggable onDragStart={(e) => handleDragStart(e, preset.id)} onDragOver={handleDragOver} onDragLeave={handleDragLeave} onDrop={(e) => handleDrop(e, preset.id)} onDragEnd={handleDragEnd} className={`presets__option ${dragItem && "dragged"}`} key={preset.id} >
                                                         {
+                                                            dropdownOpen === preset.id &&
+                                                            <Dropdown dropdownOpen={dropdownOpen} setDropdownOpen={setDropdownOpen} direction="column" anchor="right">
+                                                                <button className="dropdownBtn presets__option__main__share" onClick={(e) => {
+                                                                    e.stopPropagation();
+                                                                    setShareId(preset.id);
+                                                                    shareSnippet(user.userId, preset.id)
+                                                                }} title="Copy share link to clipboard">
+                                                                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" className="bi-share-fill" viewBox="0 0 16 16">
+                                                                        <path d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5z" />
+                                                                    </svg>
+                                                                </button>
+                                                                <button className="dropdownBtn presets__option__main__edit" onClick={(e) => {
+                                                                    e.stopPropagation();
+                                                                    setEditId(preset.id);
+                                                                    setEditName("");
+                                                                    setDeleteId(null);
+                                                                    setSelectedId(null);
+                                                                    setShareId(null);
+                                                                }} title="Rename snippet">
+                                                                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" className="bi-pencil-fill" viewBox="0 0 16 16">
+                                                                        <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
+                                                                    </svg>
+                                                                </button>
+                                                                <button className="dropdownBtn presets__option__main__del" onClick={(e) => {
+                                                                    e.stopPropagation();
+                                                                    setDeleteId(preset.id);
+                                                                    setEditId(null);
+                                                                    setSelectedId(null);
+                                                                    setEditName("");
+                                                                    setShareId(null);
+                                                                }} title="Delete snippet">
+                                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi-trash-fill" viewBox="0 0 16 16">
+                                                                        <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
+                                                                    </svg>
+                                                                </button>
+                                                            </Dropdown>
+                                                        }
+                                                        {
                                                             selectedId === preset.id && !loaded ?
                                                                 <span className="presets__option__confirm" onClick={e => e.stopPropagation()}>
                                                                     <span>Load snippet?</span>
@@ -407,7 +448,7 @@ export default function Modal({ callbacks }) {
                                                                     editId === preset.id && !renamePresetLoader ?
                                                                         (
                                                                             <span className="presets__option__confirm" onClick={e => e.stopPropagation()}>
-                                                                                <span>Rename snippet?</span>
+                                                                                <span>Rename?</span>
                                                                                 <form onSubmit={(e) => { e.preventDefault(); editSelectedPreset(preset.name, preset.id, editName) }}>
                                                                                     <input spellCheck={false} type="text" value={editName} onChange={e => { setEditName(e.target.value) }} maxLength={30} placeholder="NEW NAME" />
                                                                                     <span className="presets__option__confirm__buttons">
@@ -421,7 +462,7 @@ export default function Modal({ callbacks }) {
                                                                             <div className="loader">Loading...</div> :
                                                                             (deleteId === preset.id && !deletePresetLoader ?
                                                                                 <span className="presets__option__confirm" onClick={e => e.stopPropagation()}>
-                                                                                    <span>Delete snippet?</span>
+                                                                                    <span>Delete?</span>
                                                                                     <span className="presets__option__confirm__buttons">
                                                                                         <button onClick={(e) => { e.stopPropagation(); deleteSelectedPreset(preset.name, preset.id) }}>Yes</button>
                                                                                         <button onClick={(e) => { e.stopPropagation(); setDeleteId(null) }}>No</button>
@@ -435,38 +476,12 @@ export default function Modal({ callbacks }) {
                                                                                         </span>
                                                                                         :
                                                                                         < span className="presets__option__main">
-                                                                                            <span className="presets__option__main__buttons">
-                                                                                                <button className="presets__option__main__share" onClick={(e) => {
-                                                                                                    e.stopPropagation();
-                                                                                                    setShareId(preset.id);
-                                                                                                    shareSnippet(user.userId, preset.id)
-                                                                                                }} title="Copy share link to clipboard">
-                                                                                                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" className="bi-share-fill" viewBox="0 0 16 16">
-                                                                                                        <path d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5z" />
-                                                                                                    </svg>
-                                                                                                </button>
-                                                                                                <button className="presets__option__main__edit" onClick={(e) => {
-                                                                                                    e.stopPropagation();
-                                                                                                    setEditId(preset.id);
-                                                                                                    setEditName("");
-                                                                                                    setDeleteId(null);
-                                                                                                    setSelectedId(null);
-                                                                                                    setShareId(null);
-                                                                                                }} title="Rename snippet">
-                                                                                                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" className="bi-pencil-fill" viewBox="0 0 16 16">
-                                                                                                        <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
-                                                                                                    </svg>
-                                                                                                </button>
-                                                                                                <button className="presets__option__main__del" onClick={(e) => {
-                                                                                                    e.stopPropagation();
-                                                                                                    setDeleteId(preset.id);
-                                                                                                    setEditId(null);
-                                                                                                    setSelectedId(null);
-                                                                                                    setEditName("");
-                                                                                                    setShareId(null);
-                                                                                                }} title="Delete snippet">
-                                                                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi-trash-fill" viewBox="0 0 16 16">
-                                                                                                        <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
+                                                                                            <span className="presets__option__main__buttons topRight">
+                                                                                                <button className={`dropdownBtn presets__option__main__open ${dropdownOpen === preset.id && "active"}`} onClick={() => {
+                                                                                                    setDropdownOpen(preset.id)
+                                                                                                }} title="Open dropdown">
+                                                                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-three-dots" viewBox="0 0 16 16">
+                                                                                                        <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3" />
                                                                                                     </svg>
                                                                                                 </button>
                                                                                             </span>
